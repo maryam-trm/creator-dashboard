6157e3ba76d6fd36013bf43093f0bd1d1acbec06
maryam-trm <maryam@therocketmarketing.com>
2026-06-12 10:29:44 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        terracotta: {
+          50: '#fff4f1',
+          100: '#ffe2d8',
+          200: '#ffb59d',
+          300: '#ff8a6a',
+          400: '#ff5f37',
+          500: '#ff3d17',
+          600: '#e63412',
+          700: '#b2280e',
+          800: '#7f1b0a',
+          900: '#4c0f05'
+        }
+      }
+    }
+  },
+  plugins: []
+}
